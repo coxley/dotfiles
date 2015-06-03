8d6371e9b6ede8c6cdf01069630dcfad2453bec1
Codey Oxley <coxley@users.noreply.github.com>
2015-06-03 12:01:53 -0700

    Adding check for ~/.vim/backup

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@
 #  Vim
 
 # Install Vundle if not already installed
-if [ ! -d "~/.vim/bundle/Vundle.vim" ]
-then
-    git clone \
-        https://github.com/gmarik/Vundle.vim.git \
-        ~/.vim/bundle/Vundle.vim
-fi
+[ ! -d "~/.vim/bundle/Vundle.vim" ] && git clone \
+    https://github.com/gmarik/Vundle.vim.git \
+    ~/.vim/bundle/Vundle.vim
+
+# Need to make sure ~/.vim/backup exists otherwise config causes vim to yell
+[ ! -d "~/.vim/backup" ] && mkdir ~/.vim/backup
 
 for file in $PWD/.*(D)
 do
