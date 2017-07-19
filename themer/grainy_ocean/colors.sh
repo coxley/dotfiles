7feaddef019b0ab8b09dc0744346afc7e7e9bcc0
coxley <codey.a.oxley+os@gmail.com>
2017-07-19 15:05:40 -0700

    Editing vimrc to add w0rp/ale

---
+++ themer/grainy_ocean/colors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# colors.sh - generated palette
+foreground=#ffffff
+background=#0e0e0e
+color0=#1D201D
+color1=#607AA0
+color2=#A6B8AE
+color3=#7C8DA0
+color4=#5978A0
+color5=#538CA0
+color6=#517EA0
+color7=#558FA0
+color8=#62A2AE
+color9=#97AEC8
+color10=#7393C8
+color11=#819CC8
+color12=#6997C8
+color13=#8AA4C8
+color14=#A6B7C8
+color15=#649BC8
+colors=(
+  ${color0}
+  ${color1}
+  ${color2}
+  ${color3}
+  ${color4}
+  ${color5}
+  ${color6}
+  ${color7}
+  ${color8}
+  ${color9}
+  ${color10}
+  ${color11}
+  ${color12}
+  ${color13}
+  ${color14}
+  ${color15}
+)
