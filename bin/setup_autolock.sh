76864dd50cce7c2b76e9e4d1cee710fdce409e2e
coxley <codey.a.oxley+os@gmail.com>
2017-10-17 13:07:03 -0700

    Autolock and buckfmt

--- bin/setup_autolock.sh
+++ bin/setup_autolock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xautolock -time 5 -locker ~/bin/lock_screen -notify 20 -notifier 'xset dpms force off' &
-xautolock -time 7 -locker "systemctl suspend" &
+# xautolock -time 7 -locker "systemctl suspend" &
