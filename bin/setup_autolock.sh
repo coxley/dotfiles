460169bbf67f71e52d12b95732b0e6da506a3965
coxley <codey.a.oxley+os@gmail.com>
2018-02-22 15:53:42 -0800

    ultisnips change

--- bin/setup_autolock.sh
+++ bin/setup_autolock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xautolock -time 5 -locker ~/bin/lock_screen -notify 20 -notifier 'xset dpms force off' &
+xss-lock -- i3lock -n -c 000000 &
 # xautolock -time 7 -locker "systemctl suspend" &
